60347666e3e5f1b00cabcf988808e9fd136d22b8
Suki <suki@missallsunday.com>
2013-06-14 14:09:41 -0500

    git submodule update
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * BreezeForum
+ *
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013 Jessica González
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+require 'webroot/index.php';
