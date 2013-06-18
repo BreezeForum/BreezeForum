b07e3eb9644e82f108cda83eebeb2fb73a87785a
Suki <suki@missallsunday.com>
2013-06-18 10:27:42 -0500

    PostController
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ app/controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * BreezeForum
+ *
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright 2013, Jessica González (http://missallsunday.com)
+ * @license http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace app\controllers;
+
+use app\models\Post;
+use lithium\storage\Session;
+
+class PostController extends \lithium\action\Controller
+{
+}
